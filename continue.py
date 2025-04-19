735bea2dcc66eab14c0da4080af725830ba35a5a
amjith00 <amjith9900@gmail.com>
2025-04-19 17:28:58 +0530

    python

---
+++ continue.py
@@ -0,0 +1,16 @@
+number = 1
+while number <10:
+  print(number)
+  number +=1
+  if number == 8:
+    continue
+  print("hi")
+print("out of loop")
+
+
+
+for i in range(1,11):
+  if i==4:
+    continue
+  else:
+    print(i)
